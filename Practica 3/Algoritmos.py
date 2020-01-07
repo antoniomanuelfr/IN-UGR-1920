6a08e5efcbfe079aa2aca4d17325cf33ed691926
Antonio Manuel Fresneda Rodriguez <antoniomanuelfr@gmail.com>
2020-01-07 16:24:49 +0100

    cosas para memoria

--- Practica 3/Algoritmos.py
+++ Practica 3/Algoritmos.py
@@ -16,7 +16,7 @@ import xgboost.sklearn as xgb
 import lightgbm as lgb
 
 
-data_path = "Datos"
+data_path = "Entrega/Datos"
 image_path = "Imagenes"
 submission_path = "Submision"
 seed = 10
@@ -63,12 +63,11 @@ if __name__ == "__main__":
     grid.fit(X_train.values, y_train.values.ravel())
     print("Los mejores parametros encontrados son: {}".format(grid.best_params_))
     #"""
-
-    #"""
+    """
     print("------ XGBoost...")
     clf =xgb.XGBClassifier(nthread=8)
     params_rf = {'max_depth': [8, 10],'n_estimators': [100, 200, 300, 400] }
-    #"""
+    #
     print ("Grid Search")
     grid = GridSearchCV(clf, params_rf, cv=3, n_jobs=-1, scoring=make_scorer(f1_score, average='micro'), verbose=4)
     grid.fit(X_train.values, y_train.values.ravel())
@@ -79,6 +78,28 @@ if __name__ == "__main__":
     clf =xgb.XGBClassifier(nthread=8, **grid.best_params_)
 
     validacion_cruzada(clf, X=X_train.values, y=y_train.values.ravel(), cv=skf)
+    clf = clf.fit(X_train.values, y_train.values.ravel())
+    y_pred_tra = clf.predict(X_train.values)
+    print("F1 score (tra): {:.4f}".format(f1_score(y_train.values, y_pred_tra, average='micro')))
+    y_pred_tst = clf.predict(X_test.values)
+
+    df_submission = pd.read_csv('{}/nepal_earthquake_submission_format.csv'.format(submission_path))
+    df_submission['damage_grade'] = y_pred_tst
+    df_submission.to_csv("{}/submission.csv".format(submission_path), index=False)
+    """
+        # '''
+    print("------ Random Forest...")
+    rf = RandomForestClassifier(random_state=seed, n_jobs=-1)
+    params_rf = {'n_estimators': [20, 30, 50]}
+
+    param_f1 = {'average': 'micro'}
+    grid = GridSearchCV(rf, params_rf, cv=5, n_jobs=-1, scoring=make_scorer(f1_score, average='micro'), verbose = 4)
+    grid.fit(X_train.values, y_train.values.ravel())
+    #lgbm, y_test_lgbm = validacion_cruzada(lgbm, X_train.values, y_train.values.ravel(), skf)
+    # '''
+
+    # clf = xgbclf
+    clf = grid.best_estimator_
     clf = clf.fit(X_train.values, y_train.values.ravel())
     y_pred_tra = clf.predict(X_train.values)
     print("F1 score (tra): {:.4f}".format(f1_score(y_train.values, y_pred_tra, average='micro')))
