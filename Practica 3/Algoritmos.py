60778647cb7b9f878477af30ba81adf6116ba04b
Antonio Manuel Fresneda Rodriguez <antoniomanuelfr@gmail.com>
2020-01-26 13:09:12 +0100

    entrega

--- Practica 3/Algoritmos.py
+++ Practica 3/Algoritmos.py
@@ -10,11 +10,9 @@ from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn import preprocessing
-import xgboost.sklearn as xgb
-#import lightgbm as lgb
-
+import xgboost as xgb
+import lightgbm as lgb
 
 data_path = "Datos"
 image_path = "Imagenes"
@@ -46,39 +44,35 @@ def validacion_cruzada(modelo, X, y, cv):
 if __name__ == "__main__":
     # Lectura de datos ya procesados
 
-    X_train = pd.read_csv("{}/X_train_procesado_sampled.csv".format(data_path))
-    y_train = pd.read_csv("{}/y_train_procesado_sampled.csv".format(data_path))
-    X_test = pd.read_csv("{}/X_test_procesado_sampled.csv".format(data_path))
+    X_train = pd.read_csv("{}/X_train_procesado.csv".format(data_path))
+    y_train = pd.read_csv("{}/y_train_procesado.csv".format(data_path))
+    X_test = pd.read_csv("{}/X_test_procesado.csv".format(data_path))
 
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 
-    '''
-    print("------ AdaBoost...")
-    clf = AdaBoostClassifier(random_state=seed)
-    params_rf = {'n_estimators': [1000,110,1200,1300]}
+    #'''
+    print("------ MLP...")
+    #clf = MLPClassifier(hidden_layer_sizes=(100,100),activation= 'relu', learning_rate='adaptive',random_state=seed)
+    clf = 
+    params_rf = {'hidden_layer_sizes': [(100,60), (100,80), (100,100) ]}
     
+    param_f1 = {'average': 'micro'}
+    #grid = GridSearchCV(clf, params_rf, cv=3, n_jobs=-1, scoring=make_scorer(f1_score, average='micro'), verbose = 4)
+    #grid.fit(X_train.values, y_train.values.ravel())
+    #clf = MLPClassifier(activation= 'relu', learning_rate='adaptive',random_state=seed)
     # '''
     """
-    grid = GridSearchCV(clf, params_rf, cv=3, n_jobs=-1, scoring=make_scorer(f1_score, average='micro'), verbose=4)
-    grid.fit(X_train.values, y_train.values.ravel())
-    print("Los mejores parametros encontrados son: {}".format(grid.best_params_))
-    #"""
+    lgbm = lgb.LGBMClassifier(objective='regression_l1',n_estimators=200,n_jobs=2)
+    #lgbm, y_test_lgbm = validacion_cruzada(lgbm,X_train,y_train,skf)
+    clf = lgbm
+    #'''
 
-    #"""
-    print("------ XGBoost...")
-    clf =xgb.XGBClassifier(n_estimators = 500,  max_depth = 10, nthread=-1)
-    params_rf = {'booster':['gbtree', 'dart', 'gblinear'], 'max_depth':[10, 11]}
-    #"""
-    print ("Grid Search")
-    grid = GridSearchCV(clf, params_rf, cv=3, n_jobs=-1, scoring=make_scorer(f1_score, average='micro'), verbose=4)
-    grid.fit(X_train.values, y_train.values.ravel())
-    print("Los mejores parametros encontrados son: {}".format(grid.best_params_))
-    # Creo un modelo con los parametros anteriores
-    print("Validacion cruzada")
-    # clf = AdaBoostClassifier(random_state=seed)
-    clf =xgb.XGBClassifier(nthread=8, **grid.best_params_)
+    #clf = xgbclf
 
-    #validacion_cruzada(clf, X=X_train.values, y=y_train.values.ravel(), cv=skf)
+    """
+    # Creo un Random Forest con los parametros anteriores
+
+    validacion_cruzada(clf, X=X_train.values, y=y_train.values.ravel(), cv=skf)
     clf = clf.fit(X_train.values, y_train.values.ravel())
     y_pred_tra = clf.predict(X_train.values)
     print("F1 score (tra): {:.4f}".format(f1_score(y_train.values, y_pred_tra, average='micro')))
@@ -86,4 +80,4 @@ if __name__ == "__main__":
 
     df_submission = pd.read_csv('{}/nepal_earthquake_submission_format.csv'.format(submission_path))
     df_submission['damage_grade'] = y_pred_tst
-    df_submission.to_csv("{}/submission_sampled.csv".format(submission_path), index=False)
+    df_submission.to_csv("{}/submission.csv".format(submission_path), index=False)
